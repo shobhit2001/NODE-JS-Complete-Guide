fcfb9209c5f8c9bc77cd6fc217ce73aaf5d93b45
shobhit2001 <shobhitanand30@gmail.com>
2023-10-30 14:58:47 +0530

    Request/Response

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers);
+//     res.setHeader('content-type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page</text></html>');
+//     res.write('<body><h1>Hello</h1></body>');
+//     res.write('</html>');
+// });
+
+// server.listen(3000);
+
+
+
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+  // Parse the request URL
+  const parsedUrl = url.parse(req.url, true);
+
+  // Set the response headers
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+
+  // Determine the response based on the URL
+  if (parsedUrl.pathname === '/home') {
+    res.end('Welcome home');
+  } else if (parsedUrl.pathname === '/about') {
+    res.end('Welcome to About Us page');
+  } else if (parsedUrl.pathname === '/node') {
+    res.end('Welcome to my Node Js project');
+  } else {
+    // Handle other URLs with a generic response
+    res.end('Welcome to my Node Js project'); // You can change this message if needed
+  }
 });
 
-server.listen(4000);
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

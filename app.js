3276385e4b76708837a61eb6a719c01326681a07
shobhit2001 <shobhitanand30@gmail.com>
2024-01-10 23:40:15 +0530

    Nodemon and Package JSON

--- app.js
+++ app.js
@@ -141,60 +141,77 @@
 
 
 
-const fs = require('fs');
-const requestHandler = (req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-      });
-    });
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', chunk => {
-          console.log(chunk);
-          body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html>');
-      res.write('<head><title>My First Page</title><head>');
-      res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-      res.write('</html>');
-      res.end();  
-};
-
-//module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
+// const fs = require('fs');
+// const requestHandler = (req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     if (url === '/') {
+//         res.write('<html>');
+//         res.write('<head><title>Enter Message</title><head>');
+//         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+//         res.write('</html>');
+//         return res.end();
+//       });
+//     });
+//   }
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<html>');
+//   res.write('<head><title>My First Page</title><head>');
+//   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//   res.write('</html>');
+//   res.end();
+// });
+//     }
+//     if (url === '/message' && method === 'POST') {
+//         const body = [];
+//         req.on('data', chunk => {
+//           console.log(chunk);
+//           body.push(chunk);
+//         });
+//         return req.on('end', () => {
+//             const parsedBody = Buffer.concat(body).toString();
+//             const message = parsedBody.split('=')[1];
+//             fs.writeFile('message.txt', message, err => {
+//                 res.statusCode = 302;
+//                 res.setHeader('Location', '/');
+//                 return res.end();
+//             });
+//         });
+//     }
+//       res.setHeader('Content-Type', 'text/html');
+//       res.write('<html>');
+//       res.write('<head><title>My First Page</title><head>');
+//       res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//       res.write('</html>');
+//       res.end();  
 // };
 
-// module.exports.handler = request;
-// module.exports.someText = 'Some hard coded text';
+// //module.exports = requestHandler;
 
-exports.handler = requestHandler;
-exports.someText = 'Some hard coded text';
+// // module.exports = {
+// //     handler: requestHandler,
+// //     someText: 'Some hard coded text'
+// // };
+
+// // module.exports.handler = request;
+// // module.exports.someText = 'Some hard coded text';
+
+// exports.handler = requestHandler;
+// exports.someText = 'Some hard coded text';
+
+
+
+
+
+{
+  "name": "learning-node",
+  "version": "20.0.0" ,
+  "description": "Learning and practising",
+  "main": "app.js",
+  "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1",
+      "start": "node .js"
+  },
+  "author": "Manasi Gautam",
+  "license": "ISC"
+}
